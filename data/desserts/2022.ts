d5161607b0fdb5de95b4f1d8f1385b868d5a3441
Curt Grimes <curt@curtgrimes.com>
2022-12-13 21:08:30 -0600

    Add peanut butter cookie model

--- data/desserts/2022.ts
+++ data/desserts/2022.ts
@@ -1,3 +1,22 @@
 import { Dessert } from "interfaces";
 
-export default [] as Dessert[];
+export default [
+  {
+    name: "Dessert 1",
+    slug: "dessert-1",
+    description: "",
+    model: {
+      path: "/static/models/2022/dessert1.gltf",
+      scale: 1,
+      rotation: {
+        x: 0.1,
+        y: 3.5,
+        z: 0.3,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+] as Dessert[];
