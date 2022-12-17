36dd1ee73bc39ef19dce906cfc2d0ceb15804149
Stephen Fuchs <stephen@stephenfuchs.net>
2022-12-16 18:03:53 -0600

    add first 3 recipes

--- data/desserts/2022.ts
+++ data/desserts/2022.ts
@@ -1,3 +1,135 @@
 import { Dessert } from "interfaces";
 
-export default [] as Dessert[];
+export default [
+  {
+          name: "Chocolate Mint Stuffed Cookie",
+          slug: "chocolate-mint-stuffed-cookie",
+          description:
+            "’Tis the season to be jolly over these chocolate mint stuffed cookies. This decadent chocolate treat will help to keep your days merry and bright with a cool minty center nipping at your nose. Pair these festive cookies with a hot cup of cocoa by the fire.",
+          model: {
+            path: "/static/models/2021/chocolate-pretzel-rice-crispy.gltf",
+            scale: 0.08,
+            rotation: {
+              x: 0.3,
+              y: 1,
+              z: 0.4,
+            },
+          },
+          refrigerationRecommended: false,
+          stephensFavorite: false,
+          curtsFavorite: true,
+          recipe: `
+## Ingredients
+
+**1 &frac12; cups** all-purpose flour  
+**&frac12; cup** unsweetened cocoa powder  
+**&frac34; cup** granulated sugar  
+**&frac12; cup** unsalted butter, softened at room temperature  
+**2** large eggs  
+**&frac12; tsp** salt  
+**&frac12; tsp** baking soda  
+**12** Andies chocolate mints, broken in half  
+
+## Directions
+
+1. In a bowl, mix together the flour, cocoa powder, salt and baking soda and set it aside.
+2. In the bowl of a standing mixer fitted with a paddle attachment, cream together the butter and sugar for about a minute, add the eggs and mix until well incorporated.
+3. Add the dry ingredients and mix just until combined and your cookie dough comes together. Pop the dough in the fridge for at least an hour. 
+4. Preheat your oven to 350 degrees, line a baking sheet with some parchment paper and set aside. 
+5. Using a cookie scoop the size of one tablespoon, scoop out your dough, flatten it in your hands, place half of an Andies mint chocolate in the center of each one and carefully seal the dough around it. 
+6. Place them a couple of inches apart on your prepared baking sheet and bake the cookies for about 10 to 12 minutes or until set around the edges, allow them to cool a bit before serving. Repeat with remaining cookies. Makes around 24 cookies.
+`,
+        },
+        {
+          name: "Peanut Butter Sandwich Cookies",
+          slug: "peanut-butter-sandwhich-cookie",
+          description:
+            "Peanut butter lovers rejoice! Chestnuts aren’t the only nut roasting on an open fire. After taking a bite out of these crunchy peanut butter sandwich cookies with its creamy peanut butter center, Jolly Old St. Nick won’t be the only one craving a cold glass of milk.",
+          model: {
+            path: "/static/models/2021/death-by-chocolate-peppermint-cookie.gltf",
+            scale: 1,
+            rotation: {
+              x: 0.4,
+              y: 3.5,
+              z: 0.4,
+            },
+          },
+          refrigerationRecommended: false,
+          stephensFavorite: true,
+          curtsFavorite: false,
+          recipe: `
+## Ingredients
+
+### Cookies
+**1 &frac14; cups** dry roasted peanuts, unsalted  
+**&frac34; cup** all-purpose flour  
+**1 tsp** baking soda  
+**&frac12; tsp** salt  
+**3 Tbsp** unsalted butter, melted  
+**&frac12; cup** creamy peanut butter  
+**&frac12; cup** granulated sugar  
+**&frac12; cup** light brown sugar, packed  
+**3 Tbsp** milk  
+**1** large egg  
+
+### Filling
+**&frac34; cup** creamy peanut butter  
+**3 Tbsp** unsalted butter  
+**1 cup** powdered sugar  
+
+## Directions
+
+1. To make the cookies, preheat your oven to 350 degrees and line a baking sheet with parchment paper.
+2. Pulse peanuts in food processor until finely chopped, about 8 pulses. Whisk flour, baking soda, and salt together in bowl. Whisk butter, peanut butter, granulated sugar, brown sugar, milk, and egg together in second bowl. Stir flour mixture into peanut butter mixture with rubber spatula until combined. Stir in peanuts until evenly distributed.
+3. Using a cookie scoop or tablespoon measure, place 12 mounds, evenly spaced, on the prepared baking sheet. Using a damp hand, flatten mounds until 2 inches in diameter.
+4. Bake until deep golden brown and firm to touch, 15 to 18 minutes, rotating the sheet halfway through baking. Let cookies cool on the sheet for 5 minutes. Transfer cookies to wire rack and let cool completely, about 30 minutes. Repeat portioning and baking remaining dough.
+5. To make the filling, microwave peanut butter and butter until butter is melted and warm, about 40 seconds. Using rubber spatula, stir in powdered sugar until combined.
+5. To assemble, place half the cookies upside down on work surface. Place 1 level tablespoon of the warm filling in the center of each cookie. Place an unflipped cookie on top of filling, right side up, pressing gently until filling spreads to edges. Allow filling to set for 1 hour before serving. Makes around 24 cookies.
+`,
+        },
+  {
+    name: "Brown Butter Ginger Cookie",
+    slug: "brown-butter-ginger-cookie",
+    description:
+      "What a wonderful time of the year when the the smells of gingerbread fill the air. Enjoy these spicy, sweet, and delicious ginger cookies made with browned butter to add a caramel-like flavor that will surely add some warmth and magic to your holidays.",
+    model: {
+      path: "/static/models/2021/chunky-chocolate-peanut-butter-cookie.gltf",
+      scale: 1,
+      rotation: {
+        x: 0.4,
+        y: 3,
+        z: 0.4,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: `
+## Ingredients
+
+**2 &frac14; cups** all-purpose flour  
+**1 tsp** baking soda  
+**&frac14; tsp** salt  
+**1 tsp** cinnamon  
+**1 &frac34; tsp** ginger  
+**1 tsp** allspice  
+**&frac34; cup** butter  
+**1 cup** dark brown sugar, packed  
+**1** egg 
+**1 tsp** vanilla extract  
+**&frac14; cup** molasses  
+**&frac14; cup** granulated sugar for rolling  
+
+## Directions
+
+1. Whisk together the flour, baking soda, cinnamon, ginger, all spice, and salt in a bowl and set aside.
+2. Make your brown butter: melt butter in a saucepan over medium heat. The butter will begin to foam. Make sure you whisk consistently during this process. After a couple of minutes, the butter will begin to brown on the bottom of the saucepan; continue to whisk and remove from heat as soon as the butter begins to brown and give off a nutty aroma.
+3. Immediately transfer the butter to a bowl to prevent burning. Set aside to cool for about 5 minutes.
+4. With an electric mixer, mix the butter and brown sugar until thoroughly blended. Beat in the egg, vanilla, and molasses until smooth and creamy.
+5. Add the dry ingredients slowly and beat on low-speed just until combined. Chill dough in fridge for about 15 minutes (this is to make the dough easier to roll into balls).
+6. Preheat the oven to 350 degrees F. Grab a large tablespoon or so of dough and roll into a ball then roll in sugar. Place dough balls on cookie sheet. Bake 10-12 minutes.
+7. Remove from oven and cool the cookies on sheets about 2 minutes then transfer to a wire rack. Repeat with remaining dough. Makes 20-22 cookies.
+
+          `,
+  },
+] as Dessert[];
