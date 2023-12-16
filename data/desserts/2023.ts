f3d759932d0ae20cd09f65d4c61e9b59621ce2e4
Curt Grimes <curt@curtgrimes.com>
2023-12-16 15:39:00 -0600

    Add start of 2023 cookies

---
+++ data/desserts/2023.ts
@@ -0,0 +1,112 @@
+import { Dessert } from "interfaces";
+
+export default [
+  {
+    name: "Peanut Butter Balls",
+    slug: "peanut-butter-balls",
+    description: "",
+    model: {
+      path: "/static/models/2023/peanut-butter-balls.gltf",
+      scale: 1,
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+  {
+    name: "Peppermint Snowball Cookies",
+    slug: "peppermint-snowball-cookies",
+    description: "",
+    model: {
+      path: "/static/models/2023/peppermint-snowball-cookies.gltf",
+      scale: 1,
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+  {
+    name: "Maple Brown Sugar Cookies",
+    slug: "maple-brown-sugar-cookies",
+    description: "",
+    model: {
+      path: "/static/models/2023/maple-brown-sugar-cookies.gltf",
+      scale: 1,
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+  {
+    name: "Cookies & Cream Cookies",
+    slug: "cookies-and-cream-cookies",
+    description: "",
+    model: {
+      path: "/static/models/2023/cookies-and-cream-cookies.gltf",
+      scale: 1,
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+  {
+    name: "Pumpkin Snickerdoodles",
+    slug: "pumpkin-snickerdoodles",
+    description: "",
+    model: {
+      path: "/static/models/2023/pumpkin-snickerdoodles.gltf",
+      scale: 1,
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+  {
+    name: "Lemon Coconut Drop Shortbread Cookies",
+    slug: "lemon-coconut-drop-shortbread-cookies",
+    description: "",
+    model: {
+      path: "/static/models/2023/lemon-coconut-drop-shortbread-cookies.gltf",
+      scale: 1,
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    },
+    refrigerationRecommended: false,
+    stephensFavorite: false,
+    curtsFavorite: false,
+    recipe: ``,
+  },
+] as Dessert[];
