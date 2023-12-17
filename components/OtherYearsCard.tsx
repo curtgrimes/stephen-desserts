2afe253cf5d9387dd647bd3d314faecd2f5925b1
Curt Grimes <curt@curtgrimes.com>
2023-12-16 19:36:58 -0600

    Add 'this year' option to OtherYearsCard

--- components/OtherYearsCard.tsx
+++ components/OtherYearsCard.tsx
@@ -7,18 +7,30 @@ export default function IntroductionCard() {
 
   return (
     <div className="mb-10 flex w-full flex-col items-center justify-center gap-8">
-      <Link
-        href="/2022/chocolate-mint-stuffed-cookie"
-        className="button h-full w-full bg-amber-200/30 text-lg"
-      >
-        View Stephen&apos;s desserts from 2022 <FaChevronRight />
-      </Link>
-      <Link
-        href="/2021/chocolate-pretzel-rice-crispy"
-        className="button h-full w-full bg-amber-200/30 text-lg"
-      >
-        View Stephen&apos;s desserts from 2021 <FaChevronRight />
-      </Link>
+      {router.query.year !== "2023" && (
+        <Link
+          href="/2023/peanut-butter-balls"
+          className="button h-full w-full bg-amber-200/30 text-lg"
+        >
+          View this year&apos;s desserts <FaChevronRight />
+        </Link>
+      )}
+      {router.query.year !== "2022" && (
+        <Link
+          href="/2022/chocolate-mint-stuffed-cookie"
+          className="button h-full w-full bg-amber-200/30 text-lg"
+        >
+          View Stephen&apos;s desserts from 2022 <FaChevronRight />
+        </Link>
+      )}
+      {router.query.year !== "2021" && (
+        <Link
+          href="/2021/chocolate-pretzel-rice-crispy"
+          className="button h-full w-full bg-amber-200/30 text-lg"
+        >
+          View Stephen&apos;s desserts from 2021 <FaChevronRight />
+        </Link>
+      )}
     </div>
   );
 }
