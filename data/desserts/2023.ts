7b42e19fcaaf0d15c0117d280a268df4bfa60aba
Stephen Fuchs <stephen@stephenfuchs.net>
2023-12-16 18:47:29 -0600

    Added descriptions and recipes

--- data/desserts/2023.ts
+++ data/desserts/2023.ts
@@ -4,7 +4,8 @@ export default [
   {
     name: "Peanut Butter Balls",
     slug: "peanut-butter-balls",
-    description: "",
+    description:
+      "Dive into holiday delight with these creamy peanut spheres dipped in bittersweet chocolate. Each bite is a festive fusion, marrying peanut richness with chocolate decadence. A treat embodying enchanting seasonal magic.",
     model: {
       path: "/static/models/2023/peanut-butter-balls.gltf",
       scale: 1,
@@ -14,15 +15,35 @@ export default [
         z: 0,
       },
     },
-    refrigerationRecommended: false,
-    stephensFavorite: false,
+    refrigerationRecommended: true,
+    stephensFavorite: true,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**&frac12; cup** natural creamy peanut butter  
+**1 Tbsp** pure maple syrup  
+**1 Tbsp** unsalted butter  
+**&frac34; cup** powdered sugar  
+**1 pinch** salt  
+**1 tsp** vanilla extract  
+**3 oz** semi sweet chocolate chips  
+
+## Directions
+
+1. In a medium bowl, mix the peanut butter, maple syrup, 1 tablespoon melted butter, powdered sugar, salt and vanilla extract until a uniform dough forms. (If the dough is too sticky, you can add a teaspoon or so more butter or water to get it to come together; it should have a playdough consistency.)
+2. Roll the peanut butter dough into 1 tablespoon sized balls.
+3. Place the balls on a parchment lined baking sheet and freeze them for atleast 5 minutes while you melt the chocolate. 
+4. Meanwhile, add the chocolate chips to a dry glass bowl or liquid measuring cup and microwave on high power for 20 seconds, then stir. Microwave another 20 seconds, stir, and 20 seconds more and stir. At this point it should be mostly melted with a few chunks: keep stirring until all the chocolate chunks disappear and the chocolate is perfectly smooth. 
+5. Use two forks to place each peanut butter ball into the chocolate, rotating it to evenly cover, then remove from the chocolate, gently shake off any excess, and place it back onto the parchment lined baking sheet. 
+6. Refrigerate for 30 minutes until set. Makes around 15 peanut butter balls.
+    `,
   },
   {
     name: "Peppermint Snowballs",
     slug: "peppermint-snowballs",
-    description: "",
+    description:
+      "Step into a frosty wonderland with these buttery, peppermint-laced delights. Each crumb harbors crushed candy cane, a burst of festive cheer. Powdered snow-like sugar crowns these treats, echoing the merry spirit of the season.",
     model: {
       path: "/static/models/2023/peppermint-snowballs.gltf",
       scale: 1,
@@ -32,15 +53,42 @@ export default [
         z: 0,
       },
     },
-    refrigerationRecommended: false,
+    refrigerationRecommended: true,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**Dough:**  
+**1 cup** unsalted butter, softened to room temperature  
+**&frac34; cup** (90g) powdered sugar  
+**1 tsp** vanilla extract  
+**1 tsp** peppermint extract  
+**2 and &frac14; cups** (281g) all-purpose flour  
+**&frac18; tsp** salt  
+**&frac13; cup** (40g) crushed candy canes (about 3–4 regular-size candy canes) 
+
+
+**Coating:**  
+**1 and &frac14; cups** (150g) powdered sugar  
+
+
+## Directions
+
+1. In a large bowl using a handheld mixer or stand mixer fitted with a paddle attachment, beat the butter for 1 minute on medium speed until completely smooth and creamy. Add 3/4 cup (90g) powdered sugar and beat on low speed until sugar is incorporated, then turn the mixer up to medium-high speed and beat until combined and creamy. Scrape down the sides and bottom of the bowl as needed. Next, beat in the vanilla and peppermint extracts on medium-high speed until combined.
+2. Add the flour and salt and beat on low speed. The dough will look dry and you may not think the flour will fully combine. Once all of the flour is incorporated, turn the mixer up to high speed. The dough will come together.
+3. Add in the crushed candy canes and mix until just incorporated.
+4. Cover the cookie dough tightly and chill in the refrigerator for at least 30 minutes and up to 3 days. (If chilling for 3+ hours, make sure you let the cookie dough sit at room temperature for at least 30 minutes before rolling into balls.
+5. Preheat oven to 350°F. Line a large baking sheet with parchment paper or silicone baking mats. Set aside. Pour the powdered sugar needed for the coating into a shallow bowl.
+6. Scoop or roll 1 Tablespoon (20g) of cookie dough per cookie. Roll into a ball and place on the baking sheet, at least 2 inches apart. Bake the cookies until golden brown on the bottom edges and just barely browned on top, about 15 minutes.
+7. Allow the cookies to cool for 5 minutes on the baking sheet, then very gently roll them in the powdered sugar to coat completely. Place the cookies on wire racks to cool completely. The powdered sugar will melt a bit and get sticky. Once the cookies have completely cooled, roll in confectioners’ sugar again. This is when the sugar coating will really stick. Makes around 36 cookies.
+    `,
   },
   {
     name: "Maple Brown Sugar",
     slug: "maple-brown-sugar",
-    description: "",
+    description:
+      "Savor the toasty bliss of a fireside Christmas morning in these cookies. Their blend of dark brown sugar and maple syrup evokes the warmth of the season. Folded with toasted pecans, they mirror crackling fires and winter landscapes, a festive treat in every bite.",
     model: {
       path: "/static/models/2023/maple-brown-sugar.gltf",
       scale: 1,
@@ -53,12 +101,45 @@ export default [
     refrigerationRecommended: false,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**Dough:**  
+**2 and &frac13; cups** (291g) all-purpose flour  
+**1 tsp** baking soda  
+**&frac12; tsp** salt  
+**&frac12; cup** unsalted butter, softened to room temperature  
+**1 cup** (200g) packed dark brown sugar  
+**1** large egg, at room temperature  
+**&frac13; cup** pure maple syrup  
+**1 tsp** vanilla extract  
+**1 tsp** maple extract  
+**1 cup** (130g) chopped pecans  
+
+
+**Maple Icing:**  
+**2 Tbsp** unsalted butter  
+**&frac13; cup** pure maple syrup  
+**1 cup** (112g) sifted powdered sugar  
+
+
+## Directions
+
+1. Whisk the flour, baking soda, and salt together in a medium bowl. Set aside.
+2. Using a hand mixer or a stand mixer fitted with a paddle attachment, cream the butter and brown sugar together on medium speed until smooth, about 1-2 minutes. Add the egg and beat on high until combined, about 30 seconds. Scrape down the sides and bottom of the bowl as needed. Add the maple syrup, vanilla extract, and maple extract, then beat on high speed until combined.
+3. Pour the dry ingredients to the wet ingredients, then mix on low until combined. Add the pecans, then beat on low speed until combined. Dough will be creamy and soft.
+4. Cover and chill the dough for 2 hours in the refrigerator (and up to 3 days). If chilling for longer than a few hours, though, allow to sit at room temperature for at least 30 minutes before rolling and baking because the dough will be quite hard.
+5. Preheat oven to 350°F. Line large baking sheet with parchment paper or silicone baking mat. Set aside.
+6. Roll cookie dough into balls, about 1.5 Tablespoons of dough per cookie. Bake for 12-13 minutes until lightly browned on the sides. The centers will look very soft.
+7. Cool cookies on the baking sheet for 5 minutes before transferring to a wire rack to cool completely.
+8. Make the icing: In a small saucepan over low heat, melt the butter and maple syrup together, whisking occasionally. Once the butter has melted, remove from heat and whisk in the sifted confectioners’ sugar. Drizzle over cooled cookies. Icing will set after about 1 hour. Makes around 28 cookies.
+    `,
   },
   {
     name: "Cookies & Cream",
     slug: "cookies-and-cream",
-    description: "",
+    description:
+      "Celebrate the festive season with these joyous treats. Within, crushed Oreo delights and white chocolate chips twirl, creating a merry flavor symphony. They encapsulate the Christmas glee in every luxurious nibble.",
     model: {
       path: "/static/models/2023/cookies-and-cream.gltf",
       scale: 1,
@@ -69,14 +150,42 @@ export default [
       },
     },
     refrigerationRecommended: false,
-    stephensFavorite: false,
+    stephensFavorite: true,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**2 and &frac14; cups** (281g) all-purpose flour  
+**1 &frac12; tsps** cornstarch  
+**1 tsp** baking soda  
+**&frac12; tsp** salt  
+**4 ounces** (113g) full-fat brick cream cheese, softened to room temperature  
+**&frac34; cup** unsalted butter, softened to room temperature  
+**&frac12; cup** (100g) granulated sugar  
+**&frac12; cup** (100g) packed dark brown sugar  
+**1** large egg, at room temperature  
+**2 tsps** vanilla extract  
+**1 cup** (180g) white chocolate chips  
+**1 and &frac14; cups** roughly chopped Oreos (10 whole Oreos)  
+
+
+## Directions
+
+1. Whisk the flour, cornstarch, baking soda, and salt together in a medium bowl. Set aside.
+2. In a large bowl using a hand mixer or a stand mixer fitted with a paddle attachment, beat the cream cheese on medium-high speed until completely smooth and creamy. Add the butter and beat until combined, scraping down the sides and up the bottom of the bowl as needed.
+3. Add granulated sugar and brown sugar and beat on medium-high speed until smooth and creamy, about 2 minutes. Add the egg and vanilla and beat on high speed until combined, about 1 minute. Scrape down the sides and up the bottom of the bowl and beat again as needed to combine.
+4. Add the dry ingredients to the wet ingredients and mix on low speed until combined. With the mixer running on low speed, beat in the white chocolate chips. Add the Oreos and beat on low speed or gently fold in with a rubber spatula or wooden spoon (dough is heavy) until combined.
+5. Cover and chill the dough for at least 2 hours in the refrigerator (and up to 4 days). If chilling for longer than a few hours, allow to sit at room temperature for at least 30 minutes before rolling and baking because the dough will be quite hard.
+6. Preheat oven to 350°F. Line baking sheet with parchment paper or silicone baking mat. Set aside.
+7. Roll cookie dough, a heaping 1.5 Tablespoons of dough per cookie, and place 3 inches apart on the baking sheet. Bake for 12–13 minutes or until lightly browned on the sides. The centers will look very soft.
+8. Remove from the oven and allow cookies to cool on the baking sheet for 5 minutes before transferring to a wire rack to cool completely. Makes around 30 cookies.
+    `,
   },
   {
     name: "Pumpkin Snickerdoodles",
     slug: "pumpkin-snickerdoodles",
-    description: "",
+    description:
+      "Embrace the comforting charm of a cinnamon-spiced Christmas. Bursting with pumpkin and brown sugar sweetness, these treats exude the cozy essence of the season. Adorned with white chocolate chips, they're a festive indulgence in every bite.",
     model: {
       path: "/static/models/2023/pumpkin-snickerdoodles.gltf",
       scale: 1,
@@ -87,14 +196,48 @@ export default [
       },
     },
     refrigerationRecommended: false,
-    stephensFavorite: false,
+    stephensFavorite: true,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+### **Dough:**   
+**&frac12; cup** unsalted butter, melted & slightly cooled  
+**&frac14; cup** (50g) packed dark brown sugar  
+**&frac12; cup** (100g) granulated sugar  
+**1 tsp** vanilla extract  
+**6 Tbsp** (86g) pumpkin puree (see note)*  
+**1 and &frac12; cups** (188g) all-purpose flour  
+**&frac14; tsp** salt  
+**&frac14; tsp** baking powder  
+**&frac14; tsp** baking soda  
+**1 and &frac12; tsps** ground cinnamon  
+**1 tsp** pumpkin pie spice  
+**&frac12; cup** (90g) white chocolate chips  
+
+### **Coating:**  
+**&frac12; cup** (100g) granulated sugar  
+**1 tsp** ground cinnamon  
+
+## Directions
+
+1. Whisk the melted butter, brown sugar, and granulated sugar together in a medium bowl until no brown sugar lumps remain. Whisk in the vanilla and blotted pumpkin until smooth. Set aside.
+2. Whisk the flour, salt, baking powder, baking soda, cinnamon, and pumpkin pie spice together in a large bowl. Pour the wet ingredients into the dry ingredients and mix together with a large spoon or rubber spatula. The dough will be very soft. Fold in 1/2 cup white chocolate chips.
+3. Cover the dough and chill for 30 minutes or up to 3 days. Chilling the dough is a must for this recipe.
+4. Remove dough from the refrigerator. Preheat oven to 350°F. Line large baking sheet with parchment paper or silicone baking mat.
+5. Shape & coat the cookie dough balls: Scoop the dough, about 1.5 Tablespoons of dough per cookie, and roll each into balls. Mix the coating ingredients together, and then roll each cookie dough ball generously in the cinnamon-sugar coating. Arrange cookie dough balls 3 inches apart on the prepared baking sheet.
+6. Bake for 11–12 minutes or until the edges appear set. The cookies will look very soft in the center. Remove from the oven.
+7. Cool cookies on the baking sheet for at least 10 minutes before transferring to a wire rack to cool completely. Makes around 18 cookies.
+
+### **Note:**
+Squeeze as much of the moisture out of the pumpkin puree as you can before adding it to the cookie dough. Simply squeeze the puree with paper towels. This will help produce a less cakey cookie. Measure 6 Tablespoons AFTER the pumpkin has been squeezed/blotted.
+    `,
   },
   {
     name: "Lemon Coconut Drop Shortbread",
     slug: "lemon-coconut-drop-shortbread",
-    description: "",
+    description:
+      "Savor the zest of a joyous Christmas in these delightful cookies. Infused with tangy lemon and tropical coconut sweetness, they offer a refreshing essence of the season's cheer. These shortbread wonders capture the festive spirit, a celebration with every nibble.",
     model: {
       path: "/static/models/2023/lemon-coconut-drop-shortbread.gltf",
       scale: 1,
@@ -104,9 +247,39 @@ export default [
         z: 0,
       },
     },
-    refrigerationRecommended: false,
+    refrigerationRecommended: true,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+### **Dough:**   
+**1 cup** (80g) sweetened shredded coconut  
+**1 cup** unsalted butter, softened to room temperature  
+**&frac34; cup** (90g) confectioners’ sugar  
+**1 tsp** vanilla extract  
+**1 tsp** coconut extract  
+**1 tsp** lemon zest  
+**1 and &frac12; Tbsp** lemon juice  
+**&frac14; tsp** salt  
+**2 and &frac14; cups** (281g) all-purpose flour  
+
+### **Lemon Glaze:**  
+**1 cup** (120g) confectioners’ sugar  
+**1 and &frac12; Tbsps** lemon juice  
+**1 Tbsp** milk  
+
+## Directions
+
+1. Optional but recommended: Pulse the coconut shreds in a food processor or chop them up with a sharp knife so they are finer. Smaller pieces make a more desirable texture in the baked cookies. Set aside for next step.
+2. Using a handheld or stand mixer fitted with a paddle attachment, beat the butter for 1 minute on medium speed until completely smooth and creamy. Add the confectioners’ sugar, vanilla extract, coconut extract, and lemon zest and then beat on medium-high speed until combined. Scrape down the sides and up the bottom of the bowl and beat again as needed to combine.
+3. Add the lemon juice, salt, coconut, and flour and beat on medium speed until combined. The dough may not come together at first, but keep mixing until it does. The cookie dough will be thick.
+4. Cover the cookie dough tightly and chill in the refrigerator for 30 minutes and up to 3 days. (If chilling for 2+ hours, let the cookie dough sit at room temperature for at least 30 minutes before rolling into balls. The cookie dough will be very stiff after being in the refrigerator that long.)
+5. Preheat oven to 350°F. Line large baking sheet with parchment paper or silicone baking mat. Set aside.
+6. Roll the cookie dough into balls, about 1 Tablespoon of dough each, and place dough balls 2 inches apart on the baking sheet. If the cookie dough is too crumbly, keep rolling and working it with your hands. The warmth of your hands will help bring it together.
+7. Bake the cookies until lightly browned around the bottom edges, about 14-15 minutes.
+8. Allow the cookies to cool for 5 minutes on the baking sheet and then transfer to a wire rack. Make sure cookies are cool to the touch before dipping in icing.
+9. Make the glaze: Whisk all of the glaze ingredients together in a medium bowl. If desired, add more confectioners’ sugar to thicken or more milk to thin out. Dip cookies in icing or drizzle on top. Makes around 3 dozen cookies.
+    `,
   },
 ] as Dessert[];
