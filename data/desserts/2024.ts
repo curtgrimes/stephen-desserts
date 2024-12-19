5dc215f29dd00bde9c88968f6675285bd986af6a
Stephen Fuchs <stephen@stephenfuchs.net>
2024-12-19 11:40:54 -0600

    Add 2024 cookie descriptions and recipes

--- data/desserts/2024.ts
+++ data/desserts/2024.ts
@@ -4,7 +4,8 @@ export default [
   {
     name: "Lemon Crinkle Cookies",
     slug: "lemon-crinkle-cookies",
-    description: "",
+    description:
+      "Lemon lovers, rejoice! These tangy, melt-in-your-mouth treasures are as bright and joyful as Christmas morning. Dusted in powdered sugar like freshly fallen snow, they might just make you believe every citrusy bite was kissed by a holiday miracle.",
     model: {
       path: "/static/models/2024/lemon-crinkle-cookies.gltf",
       scale: 22,
@@ -16,14 +17,46 @@ export default [
       allowedUserRotation: "horizontal-only",
     },
     refrigerationRecommended: false,
-    stephensFavorite: false,
+    stephensFavorite: true,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**Dough:**  
+**2 &frac12; cups** (313g) all-purpose flour  
+**1 tsp** cornstarch  
+**1 tsp** baking soda  
+**&frac12; tsp** salt  
+**&frac34; cup** (12 Tbsp) unsalted butter, softened to room temperature  
+**1 cup + 2 Tbsp** (225g) granulated sugar  
+**1** large egg, at room temperature  
+**&frac14; cup** fresh or bottled lemon juice, at room temperature  
+**1 Tbsp** lemon zest  
+**1 tsp** vanilla extract  
+
+**For Rolling:**  
+**3 Tbsp** (35g) granulated sugar  
+**1 cup** (120g) confectioners’ sugar 
+
+## Directions
+
+1. Whisk the flour, cornstarch, baking soda, and salt together in a medium bowl. Set aside.  
+2. In a large bowl, use a hand mixer or stand mixer fitted with a paddle attachment to beat the butter and granulated sugar on medium-high speed until creamed, about 1 minute.  
+3. Add the egg, lemon juice, lemon zest, and vanilla extract. Beat on high speed until combined, about 1 minute. (The mixture may appear curdled—this is normal.) Scrape down the sides and bottom of the bowl.  
+4. Gradually add the flour mixture and beat on low speed until combined. The dough will be thick, creamy, and sticky.  
+5. Cover the dough and chill in the refrigerator for at least 3 hours.  
+6. Remove the dough from the refrigerator. If chilled longer than 3 hours, let it sit at room temperature for about 10 minutes to make it easier to scoop and roll.  
+7. Preheat oven to 350°F. Line baking sheets with parchment paper. Set aside.  
+8. Scoop and roll dough into 1-tablespoon-sized balls (about 20g each). Roll each ball in granulated sugar before rolling generously in confectioners’ sugar.  
+9. Place dough balls 3 inches apart on baking sheets. Bake for 12–13 minutes, or until edges appear set and centers still look soft.
+10. Cool cookies for 5 minutes on the baking sheet, and then transfer to a cooling rack to cool completely.
+    `,
   },
   {
     name: "Peanut Butter Snickerdoodles",
     slug: "peanut-butter-snickerdoodles",
-    description: "",
+    description:
+      "Santa won’t need a list to remember these peanut butter-packed favorites! Rolled in cinnamon sugar and brimming with holiday warmth, they’re as simple, sweet, and satisfying as a cup of cocoa on a snowy night.",
     model: {
       path: "/static/models/2024/peanut-butter-snickerdoodles.gltf",
       scale: 22,
@@ -37,12 +70,45 @@ export default [
     refrigerationRecommended: false,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**Dough:**  
+**1 cup + 2 Tbsp** (142g) all-purpose flour  
+**&frac12; tsp** baking soda  
+**&frac12; tsp** cream of tartar  
+**&frac14; tsp** salt  
+**&frac12; cup** (8 Tbsp) unsalted butter, softened to room temperature  
+**&frac34; cup** (150g) packed light or dark brown sugar  
+**&frac14; cup** (50g) granulated sugar  
+**1** large egg, at room temperature  
+**&frac12; cup** (135g) creamy peanut butter  
+**1 tsp** vanilla extract  
+**&frac34; cup** (110g) chopped salted peanuts  
+
+**Topping:**  
+**&frac13; cup** (70g) granulated sugar  
+**1 tsp** ground cinnamon
+
+## Directions
+
+1. Whisk the flour, baking soda, cream of tartar, and salt together in a medium bowl. Set aside.  
+2. In a large bowl, use a hand mixer or stand mixer fitted with a paddle attachment to beat the butter, brown sugar, and granulated sugar together until completely creamed and smooth, about 2 minutes.  
+3. Add the egg and beat until combined, about 1 minute. Scrape down the sides and bottom of the bowl as needed.  
+4. Add the peanut butter and vanilla extract, and beat until combined.  
+5. Gradually add the flour mixture to the wet ingredients and beat on low speed until combined. Add the chopped peanuts and beat until just incorporated. The dough will be creamy and soft.  
+6. Cover and chill the dough in the refrigerator for at least 3 hours.  
+7. Preheat oven to 350°F. Line large baking sheets with parchment paper. Set aside.  
+8. In a small bowl, mix the granulated sugar and cinnamon. Roll chilled dough into 1 heaping tablespoon (25g) sized balls. Roll each ball generously in the cinnamon sugar, then arrange on baking sheets about 3 inches apart.  
+9. Bake for 11–12 minutes or until edges are set and lightly browned. The centers will still look soft.  
+10. Let cookies cool on the baking sheet for 10 minutes before transferring to a cooling rack to cool completely.  
+    `,
   },
   {
     name: "Almond Butter Coconut Macaroons",
     slug: "almond-butter-coconut-macaroons",
-    description: "",
+    description:
+      "Sleigh into sweetness with these almond and coconut beauties! Soft, chewy, and topped with a drizzle of chocolate, they’re like the edible version of a warm tropical escape—perfect for those dreaming of both a white Christmas and a sunlit getaway.",
     model: {
       path: "/static/models/2024/almond-butter-coconut-macaroons.gltf",
       scale: 14,
@@ -53,15 +119,40 @@ export default [
       },
       allowedUserRotation: "horizontal-only",
     },
-    refrigerationRecommended: false,
+    refrigerationRecommended: true,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**Macaroons:**  
+**14-ounce** (396g) package sweetened shredded coconut (about 5 cups)  
+**&frac13; cup** (35g) almond flour  
+**&frac12; cup** (128g) almond butter  
+**4** large egg whites, at room temperature  
+**&frac12; cup** (100g) granulated sugar  
+**1 tsp** pure vanilla extract  
+
+**Topping:**  
+**4-ounce** (113g) semi-sweet chocolate bar  
+
+## Directions
+
+1. Preheat oven to 325°F. Line large baking sheets with parchment paper. Set aside.  
+2. In a food processor or blender, pulse the coconut and almond flour together until the coconut is finely chopped and the two ingredients are combined, about 10-12 pulses. Add the almond butter and pulse until the mixture is moistened and combined, about 10 more pulses. Set aside.  
+3. In a large bowl, use a handheld or stand mixer with a whisk attachment to beat the egg whites, sugar, and vanilla extract on high speed until creamy, thickened, and the sugar has mostly dissolved, about 4-5 minutes.  
+4. Using a silicone spatula or mixer on medium speed, fold/beat in the coconut almond mixture until combined and evenly moistened. Dough will be wet and sticky.  
+5. Using a medium cookie scoop or spoon, scoop 1.5 tablespoons of dough and place on prepared baking sheets, spacing them at least 3 inches apart.  
+6. Bake for about 20 minutes, or until lightly golden brown around the edges.  
+7. Cool for 10 minutes on the baking sheets before transferring to a wire rack to cool completely.  
+8. Melt the chocolate in a double boiler or microwave in 15-second increments, stirring after each, until smooth. Drizzle melted chocolate over cooled cookies. Let the chocolate set in the refrigerator or at room temperature.
+    `,
   },
   {
     name: "Peppermint White Chocolate Cookies",
     slug: "peppermint-white-chocolate-cookies",
-    description: "",
+    description:
+      "Refreshingly minty and decadently sweet, these cookies are pure holiday magic. Loaded with creamy white chocolate and festive candy cane crunch, they capture the joy of the season—like a winter wonderland in every bite.",
     model: {
       path: "/static/models/2024/peppermint-white-chocolate-cookies.gltf",
       scale: 19,
@@ -75,12 +166,43 @@ export default [
     refrigerationRecommended: false,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**2 cups** (250g) all-purpose flour  
+**1 tsp** cornstarch  
+**1 tsp** baking soda  
+**&frac12; tsp** salt  
+**&frac34; cup** (12 Tbsp) unsalted butter, softened to room temperature  
+**&frac12; cup** (100g) packed light brown sugar  
+**&frac12; cup** (100g) granulated sugar  
+**1** large egg, at room temperature  
+**1 &frac12; tsp** pure vanilla extract  
+**&frac34; tsp** peppermint extract  
+**&frac34; cup** (135g) white chocolate chips  
+**&frac12; cup** crushed candy cane pieces (about 5 candy canes)
+
+## Directions
+
+1. Whisk the flour, cornstarch, baking soda, and salt together in a medium bowl.  
+2. In a large bowl, use a handheld mixer or stand mixer with a paddle attachment to beat the butter on medium speed for 1 minute until smooth and creamy.  
+3. Add the brown sugar and granulated sugar to the butter and mix on medium speed until fluffy and light in color.  
+4. Mix in the egg, vanilla extract, and peppermint extract. Scrape down the sides and bottom of the bowl as needed.  
+5. On low speed, slowly mix the dry ingredients into the wet ingredients until combined. The dough will be quite thick.  
+6. Add the white chocolate chips and crushed candy cane, mixing for about 5 seconds until incorporated.  
+7. Cover the dough and chill for at least 2 hours.  
+8. Once chilled, remove the dough from the refrigerator and let it sit at room temperature for 10 minutes.  
+9. Preheat oven to 350°F and line baking sheets with parchment paper. Set aside.  
+10. Roll the dough into 1.5 Tablespoon balls and place them on the baking sheets.  
+11. Bake for 12 minutes, or until lightly golden brown around the edges.  
+12. Allow the cookies to cool for 5 minutes on the baking sheets before transferring them to a cooling rack to cool completely.  
+    `,
   },
   {
     name: "Pistachio Drop Cookies",
     slug: "pistachio-drop-cookies",
-    description: "",
+    description:
+      "Indulge in the buttery bliss of these pistachio gems, kissed with the nutty richness of brown butter icing. Like a quiet snowfall on a winter’s night, their flavor is subtle yet unforgettable—a true holiday treasure.",
     model: {
       path: "/static/models/2024/pistachio-drop-cookies.gltf",
       scale: 16,
@@ -91,15 +213,51 @@ export default [
       },
       allowedUserRotation: "horizontal-only",
     },
-    refrigerationRecommended: false,
+    refrigerationRecommended: true,
     stephensFavorite: false,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**Cookies:**  
+**1 cup** (130g) salted pistachios*  
+**1 cup** (16 Tbsp) unsalted butter, softened to room temperature  
+**&frac34; cup** (90g) confectioners’ sugar  
+**1 tsp** pure vanilla extract  
+**1 tsp** almond extract  
+**2 &frac14; cups** (281g) all-purpose flour  
+
+**Brown Butter Icing:**  
+**&frac14; cup** (4 Tbsp) unsalted butter  
+**1 cup** (120g) confectioners’ sugar  
+**2 Tbsp** milk  
+**&frac14; tsp** pure vanilla extract 
+
+## Directions
+
+1. Pulse pistachios in a food processor until small crumbs form, yielding about 3/4 cup of finely chopped pistachios. Set aside.  
+2. Beat the butter on medium speed in a handheld or stand mixer fitted with a paddle attachment for 1 minute until smooth and creamy.  
+3. Add confectioners' sugar, vanilla extract, and almond extract, then beat on medium-high speed until fully combined.  
+4. Add flour and 3/4 cup pistachio crumbs and beat until combined. The dough will be thick and may not come together immediately, but continue mixing.  
+5. Cover the dough and chill in the refrigerator for 30 minutes. (If chilling for 2+ hours, let the dough sit at room temperature for 30 minutes before rolling into balls.)  
+6. Preheat oven to 350°F and line baking sheets with parchment paper.  
+7. Roll dough into 1 Tablespoon balls and place them 1 inch apart on baking sheets.  
+8. Bake for 14-15 minutes, until lightly browned on the bottom edges and just barely browned on top.  
+9. Cool cookies on the baking sheet for 5 minutes, then transfer to a wire rack. Let cool completely before icing.
+
+**Brown Butter Icing:**  
+1. Slice butter and place it in a small light-colored pot. Melt over medium heat, stirring occasionally.  
+2. Once melted, the butter will foam. Stir for 5-6 minutes until the butter browns and a nutty aroma forms.  
+3. Remove from heat immediately and let cool for 5 minutes.  
+4. Whisk in confectioners' sugar, milk, and vanilla extract until smooth. Adjust consistency by adding more sugar for thickness or milk to thin it.  
+5. Dip cookies into the icing or drizzle over the top. Icing will set in 1-2 hours if coated lightly.
+    `,
   },
   {
     name: "Chocolate Chili Cookies",
     slug: "chocolate-chili-cookies",
-    description: "",
+    description:
+      "Heat up your holiday with a touch of the unexpected! These chocolaty cookies have a sneaky kick—just a little spicy, a little sweet, and full of holiday magic. Perfect for anyone who believes the best presents have surprises inside.",
     model: {
       path: "/static/models/2024/pistachio-drop-cookies.gltf",
       scale: 16,
@@ -111,8 +269,41 @@ export default [
       allowedUserRotation: "horizontal-only",
     },
     refrigerationRecommended: false,
-    stephensFavorite: false,
+    stephensFavorite: true,
     curtsFavorite: false,
-    recipe: ``,
+    recipe: `
+## Ingredients
+
+**&frac12; cup** dried currants  
+**2 Tbsp** coffee flavored liqueur (Kahlua®)  
+**4 ounces** unsweetened chocolate  
+**2 ounces** bittersweet chocolate  
+**3 Tbsp** unsalted butter  
+**&frac12; cup** all-purpose flour  
+**&frac12; tsp** freshly ground black pepper  
+**&frac14; tsp** baking powder  
+**&frac14; tsp** salt  
+**&frac18; tsp** ground cinnamon  
+**&frac18; tsp** cayenne pepper  
+**&frac34; cup** sugar  
+**2** eggs  
+**2 tsps** vanilla extract  
+**1 cup** dark chocolate chips  
+
+
+## Directions
+
+1. Preheat oven to 350°F. Line baking sheets with parchment paper or silicone baking mats.
+2. Heat currants and coffee liqueur in a saucepan over low heat until it begins to simmer, about 2 minutes. Remove from heat and set aside.
+3. Combine unsweetened chocolate, bittersweet chocolate, and butter in a bowl. Place the bowl on top of a saucepan filled with 1-inch of water set over low heat. Stir occasionally until melted, about 5 minutes. Remove from heat and set aside.
+4. Mix flour, black pepper, baking powder, salt, cinnamon, and cayenne pepper in a large bowl and set aside.
+5. Whisk sugar and eggs in a small bowl until light, fluffy, and pale yellow, about 5 minutes. Slowly whisk in vanilla and melted chocolate mixture.
+6. Fold the flour mixture into the sugar and chocolate mixture until combined.
+7. Stir in dark chocolate chips and liqueur-soaked currants. Chill for 30 minutes.
+8. Drop 1.5 tablespoons of cookie dough 2 inches apart onto prepared baking sheets.
+9. Bake in the preheated oven until cookies are almost set, about 12 minutes.
+10. Remove from the oven and leave on baking sheets to cool for 5 minutes.
+11. Transfer to cooling rack and allow to finish cooling.
+    `,
   },
 ] as Dessert[];
